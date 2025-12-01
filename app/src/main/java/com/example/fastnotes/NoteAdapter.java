dfb00bedde0dd64994f8fc8eaa6f700f1245b559
EmelyanovPavel <pavelemelyanov06@gmail.com>
2025-12-01 15:38:58 +0300

    Homework #13 and homework #14

--- app/src/main/java/com/example/fastnotes/NoteAdapter.java
+++ app/src/main/java/com/example/fastnotes/NoteAdapter.java
@@ -1,22 +1,23 @@
 package com.example.fastnotes;
 
-import android.content.Context;
+//import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
-//public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
-public class NoteAdapter extends ArrayAdapter<Note> {
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
+//public class NoteAdapter extends ArrayAdapter<Note> {
+    //Homework #13
     private List<Note> notes;
+
     //    Homework 14
     private OnItemClickListener listener;
     private OnItemLongClickListener longClickListener;
-    private Context context;
+//    private Context context;
 
     public interface OnItemClickListener {
         void onItemClick(Note note);
@@ -25,56 +26,45 @@ public class NoteAdapter extends ArrayAdapter<Note> {
     public interface OnItemLongClickListener {
         boolean onItemLongClick(Note note);
     }
-
-    //    public NoteAdapter(MainActivity mainActivity, List<Note> notes) {
-//        this.notes = notes;
-//        //Homework 14
-//        this.listener = listener;
-//        this.longClickListener = longClickListener;
-//    }
-    public NoteAdapter(Context context, List<Note> notes) {
-        super(context, 0, notes);
-        this.context = context;
+//    homework #14
+    public NoteAdapter(MainActivity2 mainActivity2, List<Note> notes) {
+        this.notes = notes;
+        //Homework 14
+        this.listener = listener;
+        this.longClickListener = longClickListener;
+    }
+//    homework #13
+    public NoteAdapter(List<Note> notes) {
+//        super(context, 0, notes);
         this.notes = notes;
     }
 
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(
-                    android.R.layout.simple_list_item_1, parent, false);
-        }
-
-        Note note = notes.get(position);
-        TextView textView = convertView.findViewById(android.R.id.text1);
-        textView.setText(note.getTitle());
-        return convertView;
+//    Homework #13
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_note, parent, false);
+        return new NoteViewHolder(view);
     }
 
-//    @NonNull
-//    @Override
-//    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        View view = LayoutInflater.from(parent.getContext())
-//                .inflate(R.layout.item_note, parent, false);
-//        return new NoteViewHolder(view);
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
-//        Note note = notes.get(position);
-//        holder.tvTitle.setText(note.getTitle());
-//        holder.tvContent.setText(note.getContent());
-//    }
-//    @Override
-//    public int getItemCount() {
-//        return notes.size();
-//    }
-//
-//    static class NoteViewHolder extends RecyclerView.ViewHolder {
-//        TextView tvTitle, tvContent;
-//        public NoteViewHolder(@NonNull View itemView) {
-//            super(itemView);
-//            tvTitle = itemView.findViewById(R.id.tvTitle);
-//            tvContent = itemView.findViewById(R.id.tvContent);
-//        }
-//    }
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        Note note = notes.get(position);
+        holder.tvTitle.setText(note.getTitle());
+        holder.tvContent.setText(note.getContent());
+    }
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    static class NoteViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle, tvContent;
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvContent = itemView.findViewById(R.id.tvContent);
+        }
+    }
 }
